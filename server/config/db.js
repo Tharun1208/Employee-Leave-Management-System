e898b112da8aaa75da71d25b1a2859d851c2c840
Tharun <tharunhs1208@gmail.com>
2026-07-30 22:13:10 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,18 +1,16 @@
+require("dotenv").config();
 const mysql = require("mysql2");
-
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Tharun1208",
-    database: "employee_leave_system"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
-
-db.connect((err) => {
-    if (err) {
-        console.log("Database Connection Failed:", err);
-    } else {
+db.connect((err)=>{
+    if(err){
+        console.log("Database Connection Failed:",err);
+    }else{
         console.log("MySQL Database Connected");
     }
 });
-
 module.exports = db;
